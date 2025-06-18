61a4e822b4551853c5bb689e9bc3a66d8408864e
Fredrik <fredrik101@live.no>
2025-06-18 23:55:27 +0200

    Bind server to 0.0.0.0 so Internet can reach it

--- server.js
+++ server.js
@@ -35,5 +35,5 @@ io.on('connection', sock => {
   });
 });
 
-httpServer.listen(3000, () =>
-  console.log('✓  Server running at http://localhost:3000'));
+httpServer.listen(3000, '0.0.0.0', () =>
+  console.log('✓ Server running on port 3000 (all interfaces)'));
